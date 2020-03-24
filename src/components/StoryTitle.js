eb3f7111ee0715029ee54cf0fc66b0825152c5eb
benjewman <bennyadave@gmail.com>
2020-03-24 18:26:51 -0400

    Move blurb to StoryTitle and start to style in App.css

--- src/components/StoryTitle.js
+++ src/components/StoryTitle.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import '../App.css';
 
 function StoryTitle({story}) {
     const title = story.title
-    
+
+    // extract a small blurb (first 15 chars) from the first chapter
+    const blurb = story.chapters[0].content.slice(0, 15);
+
     return (
         <React.Fragment>
-            <h3>{title}</h3>
+            <h3 className="storyTitle">{title}</h3>
+            <p className="storyBlurb">("{blurb}...")</p>
         </React.Fragment>
     )
 }
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -11,23 +11,11 @@ function Main() {
         .then(tales => setStories(tales))
     }, []);
 
-    // function to return a small blurb (first 15 chars) from the first chapter of the story passed in
-    const storyBlurb = story => story.chapters[0].content.slice(0, 15);
-
-    // map through stories to create an h3 for each title and a blurb of its content
+    // map through stories to create a StoryTitle for each
     const renderStories = () => {
         console.log(stories)
         return (
             <React.Fragment>
-                {/* style below so the title is bigger and underlined */}
-                {/* {stories.map(story => {
-                    return(
-                    <React.Fragment>
-                    <h3 key={story.id}>{story.title} </h3>  
-                    <h3 key={}>"{storyBlurb(story)}..."</h3>
-                    </React.Fragment>
-                    )
-                })} */}
                 {stories.map(story => <StoryTitle key={story.id} story={story}/>)}
             </React.Fragment>
         )
