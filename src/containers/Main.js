3b0496538b6ebb0530b6d722a5ca3db019a11858
benjewman <bennyadave@gmail.com>
2020-03-24 17:25:15 -0400

    Add key to story title h3 tags and trim useEffect

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -7,29 +7,21 @@ function Main() {
     useEffect(() => {
         fetch('http://localhost:3000/stories')
         .then(resp => resp.json())
-        .then(tales => {
-            console.log(tales);
-            // save stories in state
-            setStories(tales)
-            // tales.forEach(story => setStories(...stories, story));
-        })
+        .then(tales => setStories(tales))
     }, []);
 
     const renderStories = () => {
         console.log(stories)
         return (
             <React.Fragment>
-                <h3>Hi</h3>
-                {stories.map(story => {
-                    return <h3>"{story.title}"</h3>
-                })}
+                {stories.map(story => <h3 key={story.id} >"{story.title}"</h3>)}
             </React.Fragment>
         )
     }
     
     return (
         <div>
-            <h2>Main Page</h2>
+            <h2>Stories</h2>
             {/* fetches most recently finished stories */}
             {/* try to batch the fetches later */}
             {renderStories()}
