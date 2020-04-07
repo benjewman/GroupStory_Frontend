ba16a4c275df52a0c2413c142c336be0cacd6307
benjewman <bennyadave@gmail.com>
2020-04-06 21:45:53 -0400

    Set up useEffect in WriteStory component to fetch last story

--- src/containers/WriteStory.js
+++ src/containers/WriteStory.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function WriteStory() {
+    // create state variable for the unfinished story or the new story
+    // set this variable from the fetch
+    const [story, setStory] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/last')
+        .then(resp => resp.json())
+        .then(data => console.log(data))
+    }, [])
+    
     return (
         <div>
             <h2>Write Story</h2>
