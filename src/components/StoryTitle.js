f48faf1a6cf6ca71094098ed769cf18bf808ae11
benjewman <bennyadave@gmail.com>
2020-04-15 20:25:16 -0400

    Add word count to StoryTitle on Main page

--- src/components/StoryTitle.js
+++ src/components/StoryTitle.js
@@ -3,11 +3,12 @@ import '../App.css';
 import { useHistory } from "react-router-dom";
 
 function StoryTitle({story}) {
-    const title = story.title;
-
     const history = useHistory();
+
+    const wordCount = story.word_count;
+    const title = story.title;
     // extract a small blurb (first 15 chars) from the first chapter
-    const blurb = story.chapters[0].content.slice(0, 15);
+    const blurb = story.chapters[0].content.slice(0, 40);
 
     // function to push to ShowStory if clicked
     const routeToStory = () => history.push(`/stories/${story.id}`);
@@ -17,7 +18,7 @@ function StoryTitle({story}) {
         // add css to make it look clickable
         <React.Fragment>
             <h3 className="storyTitle" onClick={routeToStory}>{title}</h3>
-            <p className="storyBlurb">("{blurb}...")</p>
+            <p className="storyBlurb">("{blurb}...") ({wordCount} words)</p>
             <br/>
         </React.Fragment>
     )
